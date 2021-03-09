7da8aea931089479a6252188b8c29184d2642bfe
crocens98 <crocens98@gmail.com>
2021-03-09 13:05:48 +0300

    modify hello world

--- dags/test_pipline.py
+++ dags/test_pipline.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator
 
 
 def print_hello():
