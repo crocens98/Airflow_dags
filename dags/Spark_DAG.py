3202606d3eed8c13d0914aea684e8a88a561b964
crocens98 <crocens98@gmail.com>
2021-03-10 23:14:57 +0300

    spark task ssh impl

--- dags/Spark_DAG.py
+++ dags/Spark_DAG.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
-from airflow.models import Variable
+
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.contrib.hooks.ssh_hook import SSHHook
-password_variable = 'SSH_PASSWORD'
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.bash_operator import BashOperator
+
 default_args = {
   'owner': 'crocens98',
   'depends_on_past': False,
@@ -25,19 +25,17 @@ bash_task_first = BashOperator(
     bash_command='echo "Before Spark"',
     dag=dag
 )
-sshHook = SSHHook(ssh_conn_id ='ssh_connection')
+sshHook = SSHHook(ssh_conn_id='ssh_connection')
 
-password = Variable.get(password_variable)
 spark_task = SSHOperator(
     task_id='spark_task',
     ssh_hook=sshHook,
-    command=f'''docker exec  hadoop-container /root/hadoop/spark-2.4.7-bin-hadoop2.7/bin/spark-submit \
-    --master spark://hadoop-host:7077 \
-    --deploy-mode cluster \
+    command=f'''docker exec  hadoop-container  \
+    /root/hadoop/spark-2.4.7-bin-hadoop2.7/bin/spark-submit \
+    --master spark://hadoop-host:7077  \
     --class by.zinkov.App \
     --packages org.apache.spark:spark-avro_2.11:2.4.4 \
-    --total-executor-cores 4 \
-    --executor-cores 2 \
+    --total-executor-cores 4 --executor-cores 2 \
     --executor-memory 2g \
     --driver-memory 2g \
     --name spark_task \
@@ -46,15 +44,11 @@ spark_task = SSHOperator(
     dag=dag,
 )
 
-
 bash_task_second = BashOperator(
     task_id="after_spark",
     bash_command='echo "After Spark"',
     dag=dag
 )
 
-
 bash_task_first >> spark_task
 spark_task >> bash_task_second
-
-
